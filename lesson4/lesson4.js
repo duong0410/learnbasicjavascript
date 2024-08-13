d4dffc279c2a27be451099370e43a7956f7ea4a2
duong0410 <duongstranger@gmail.com>
2024-08-13 20:43:49 +0700

    source code lesson6

--- lesson4/lesson4.js
+++
@@ -1,5 +0,0 @@
-let a = `đây là một String`;
-console.log(a)
-console.log(4+5+5+'a')
-console.log('a'+5+4)
-
---
+++ lesson6/lesson6.js
@@ -0,0 +1,14 @@
+console.log("Hello world from HTML".toUpperCase())
+let a = 5;
+let b = 6;
+let c = 6.9;
+let d = "5"
+let e = Number(d);
+let f = +d;
+console.log(a + b)
+console.log(a + c)
+console.log(a * d)
+console.log(a - d)
+console.log(a === d, `a:`, typeof a, `d:`, typeof d )
+console.log('e value: ', e, 'type of e:', typeof e)
+console.log('f value: ', f, 'type of f:', typeof f)
